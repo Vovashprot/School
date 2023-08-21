52e4d6f132aefa2009bbfcf979e0d5e1c75f606c
Vovashprot <vova.protasenko@gmail.com>
2023-08-22 00:03:18 +0300

    services

---
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -0,0 +1,10 @@
+package ru.hogwarts.school.service;
+
+import ru.hogwarts.school.model.Faculty;
+
+import java.util.HashMap;
+
+public class FacultyService {
+    HashMap<Long,Faculty> FacultyMap;
+    Long counter = 0L;
+}
---
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -0,0 +1,10 @@
+package ru.hogwarts.school.service;
+
+import ru.hogwarts.school.model.Student;
+
+import java.util.HashMap;
+
+public class StudentService {
+    HashMap<Long, Student> StudentMap;
+    Long counter = 0L;
+}
